a026c0135df18b3c6d54c4c14e167ef7a76fa979
Victor Olatunji <victoluolatunji@gmail.com>
2024-02-28 10:28:48 +0100

    init js

---
+++ script.js
@@ -0,0 +1,74 @@
+const products = [
+  {
+    name: "sony playstation 5",
+    url: "images/playstation_5.png",
+    categories: "games",
+    price: 499.99,
+  },
+  {
+    name: "samsung galaxy",
+    url: "images/samsung_galaxy.png",
+    categories: "smartphones",
+    price: 399.99,
+  },
+  {
+    name: "samsung galaxy",
+    url: "images/samsung_galaxy.png",
+    categories: "smartphones",
+    price: 399.99,
+  },
+  {
+    name: "samsung galaxy",
+    url: "images/samsung_galaxy.png",
+    categories: "smartphones",
+    price: 399.99,
+  },
+  {
+    name: "samsung galaxy",
+    url: "images/samsung_galaxy.png",
+    categories: "smartphones",
+    price: 399.99,
+  },
+  {
+    name: "samsung galaxy",
+    url: "images/samsung_galaxy.png",
+    categories: "smartphones",
+    price: 399.99,
+  },
+  {
+    name: "samsung galaxy",
+    url: "images/samsung_galaxy.png",
+    categories: "smartphones",
+    price: 399.99,
+  },
+  {
+    name: "samsung galaxy",
+    url: "images/samsung_galaxy.png",
+    categories: "smartphones",
+    price: 399.99,
+  },
+  {
+    name: "samsung galaxy",
+    url: "images/samsung_galaxy.png",
+    categories: "smartphones",
+    price: 399.99,
+  },
+  {
+    name: "samsung galaxy",
+    url: "images/samsung_galaxy.png",
+    categories: "smartphones",
+    price: 399.99,
+  },
+  {
+    name: "samsung galaxy",
+    url: "images/samsung_galaxy.png",
+    categories: "smartphones",
+    price: 399.99,
+  },
+  {
+    name: "samsung galaxy",
+    url: "images/samsung_galaxy.png",
+    categories: "smartphones",
+    price: 399.99,
+  },
+];
